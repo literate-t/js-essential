7c7a3e923e00ca05ac9d1c0a70271a62d141cba4
literate <gaekoon@gmail.com>
2022-01-30 02:57:25 +0900

    지속적으로 수정 작업 중

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
+const container = document.getElementById("root");
 const ajax = new XMLHttpRequest();
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
-ajax.open("GET", NEWS_URL, false);
-ajax.send();
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const content = document.createElement("div");
 
-// 응답 값을 객체로(json이라서 가능)
-const newsFeed = JSON.parse(ajax.response);
+function getData(url) {
+    ajax.open("GET", url, false);
+    ajax.send();
 
-console.log(newsFeed);
-const ul = document.createElement("ul");
-document.getElementById("root").appendChild(ul);
-for (let i = 0; i < newsFeed.length; ++i) {
-    const li = document.createElement("li");
-    const a = document.createElement("a");
-
-    a.innerHTML = newsFeed[i].title;
-    
-    li.appendChild(a);    
-    ul.appendChild(li);
+    return JSON.parse(ajax.response);
 }
-document.getElementById("root").appendChild(ul);
+
+const newsFeed = getData(NEWS_URL);
+
+window.addEventListener("hashchange", ()=>{
+    const id = location.hash.substring(1);
+    const contentUrl = CONTENT_URL.replace("@id", id);
+
+    const newsContent = getData(contentUrl);
+
+    const h1 = document.createElement("h1");
+    //console.log(newsContent.title);
+    h1.innerText = newsContent.title;
+    content.appendChild(h1);
+});
+
+const ul = document.createElement("ul");
+for (let i = 0; i < newsFeed.length; ++i) {
+    const div = document.createElement("div");    
+    div.innerHTML = `
+        <li>
+            <a href="#${newsFeed[i].id}">
+                ${newsFeed[i].title} (${newsFeed[i].comments_count})
+            </a>
+        </li>
+    `;
+    //ul.appendChild(div.children[0]);    
+    ul.appendChild(div.firstElementChild);
+}
+container.appendChild(ul);
+container.appendChild(content);
